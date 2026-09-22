3a5d7b5773e88f3709c064d7164a4666eea5911c
stefanla <stefanla@student.chalmers.se>
2026-09-22 18:21:36 +0200

    buffer manager additions

--- src/storage/buffer_manager.cpp
+++ src/storage/buffer_manager.cpp
@@ -3,23 +3,98 @@
 #include "disk_manager.h"
 #include "page.h"
 
+#include <stdexcept>
+#include <string>
+
 BufferManager::BufferManager(
-    DiskManager& disk_manager
+    DiskManager& disk_manager,
+    std::size_t pool_size
 )
-    : disk_manager_(disk_manager) {
+    : disk_manager_(disk_manager),
+      pool_size_(pool_size),
+      frames_(pool_size) {
+
+    free_list_.reserve(pool_size);
+    for (FrameId i = 0; i < pool_size; ++i) {
+        free_list_.push_back(i);
+    }
+}
+
+PageId BufferManager::allocatePage() {
+    return next_page_id_++;
+}
+
+FrameId BufferManager::advanceClock() {
+    FrameId hand = clock_hand_;
+    clock_hand_ = (clock_hand_ + 1) % pool_size_;
+    return hand;
+}
+
+std::optional<FrameId> BufferManager::findVictimFrame() {
+    for (std::size_t attempts = 0; attempts < 2 * pool_size_; ++attempts) {
+        FrameId candidate = advanceClock();
+        Frame& frame = frames_[candidate];
+
+        if (!frame.in_use || frame.pin_count > 0) {
+            continue;
+        }
+
+        if (frame.reference_bit) {
+            frame.reference_bit = false;
+            continue;
+        }
+
+        return candidate;
+    }
+
+    return std::nullopt;
+}
+
+FrameId BufferManager::allocateFrame() {
+    if (!free_list_.empty()) {
+        FrameId id = free_list_.back();
+        free_list_.pop_back();
+        return id;
+    }
+
+    std::optional<FrameId> victim = findVictimFrame();
+    if (!victim.has_value()) {
+        throw std::runtime_error(
+            "BufferManager: pool exhausted, every frame is pinned");
+    }
+
+    Frame& frame = frames_[*victim];
+
+    if (frame.dirty) {
+        // TODO: disk_manager_.writePage(frame.page_id, *frame.page);
+    }
+
+    page_table_.erase(frame.page_id);
+    frame.page.reset();
+    frame.page_id = INVALID_PAGE_ID;
+    frame.pin_count = 0;
+    frame.dirty = false;
+    frame.reference_bit = false;
+    frame.in_use = false;
+
+    return *victim;
 }
 
 PageId BufferManager::newPage(
     std::unique_ptr<Page> page
 ) {
-    PageId page_id = next_page_id_++;
+    PageId page_id = allocatePage();
+    FrameId frame_id = allocateFrame();
 
-    Frame frame;
+    Frame& frame = frames_[frame_id];
     frame.page = std::move(page);
+    frame.page_id = page_id;
     frame.pin_count = 1;
-    frame.dirty = true;
+    frame.dirty = true;   // a brand-new page has never been written to disk
+    frame.reference_bit = true;
+    frame.in_use = true;
 
-    pages_[page_id] = std::move(frame);
+    page_table_[page_id] = frame_id;
 
     return page_id;
 }
@@ -27,42 +102,72 @@ PageId BufferManager::newPage(
 Page* BufferManager::fetchPage(
     PageId page_id
 ) {
-    auto it = pages_.find(page_id);
+    auto it = page_table_.find(page_id);
 
-    if (it == pages_.end()) {
+    if (it == page_table_.end()) {
         return nullptr;
     }
 
-    it->second.pin_count++;
+    Frame& frame = frames_[it->second];
+    frame.pin_count++;
+    frame.reference_bit = true;
 
-    return it->second.page.get();
+    return frame.page.get();
 }
 
 void BufferManager::unpinPage(
     PageId page_id,
     bool dirty
 ) {
-    auto it = pages_.find(page_id);
+    auto it = page_table_.find(page_id);
 
-    if(it == pages_.end()){
+    if (it == page_table_.end()) {
         return;
     }
 
-    Frame& frame = it->second;
+    Frame& frame = frames_[it->second];
 
-    if(frame.pin_count > 0){
+    if (frame.pin_count > 0) {
         frame.pin_count--;
     }
 
-    if(dirty){
+    if (dirty) {
         frame.dirty = true;
     }
 }
 
-void BufferManager::flushFile(){
-    for(auto& [page_id, frame]:pages_){
-        if(frame.dirty){
-            // TODO disk_manager_.writepage(...)
+void BufferManager::disposePage(PageId page_id) {
+    auto it = page_table_.find(page_id);
+
+    if (it == page_table_.end()) {
+        return;
+    }
+
+    FrameId frame_id = it->second;
+    Frame& frame = frames_[frame_id];
+
+    if (frame.pin_count > 0) {
+        throw std::runtime_error(
+            "BufferManager::disposePage: page " + std::to_string(page_id) +
+            " is still pinned");
+    }
+
+    page_table_.erase(it);
+
+    frame.page.reset();
+    frame.page_id = INVALID_PAGE_ID;
+    frame.pin_count = 0;
+    frame.dirty = false;
+    frame.reference_bit = false;
+    frame.in_use = false;
+
+    free_list_.push_back(frame_id);
+}
+
+void BufferManager::flushPage() {
+    for (Frame& frame : frames_) {
+        if (frame.in_use && frame.dirty) {
+            // TODO: disk_manager_.writePage(frame.page_id, *frame.page);
             frame.dirty = false;
         }
     }
--- src/storage/buffer_manager.h
+++ src/storage/buffer_manager.h
@@ -4,20 +4,30 @@
 
 #include <memory>
 #include <unordered_map>
+#include <cstddef>
+#include <optional>
+#include <vector>
 
 class DiskManager;
 class BPlusTreePage;
 
+using FrameId = std::size_t;
+
 struct Frame{
     std::unique_ptr<Page> page;
-
+    PageId page_id = INVALID_PAGE_ID;
     size_t pin_count = 0;
     bool dirty = false;
+    bool reference_bit = false;
+    bool in_use = false;
 };
 
 class BufferManager {
 public:
-    explicit BufferManager(DiskManager& disk_manager);
+    explicit BufferManager(
+        DiskManager& disk_manager,
+        std::size_t pool_size = 16
+    );
 
     Page* fetchPage(PageId page_id);
 
@@ -30,34 +40,24 @@ public:
         bool dirty
     );
 
-    void advanceClock();
+    void disposePage(PageId page_id);
 
-    void allocateFrame();
-
-    void readPage(
-        PageId page_id,
-        bool dirty
-    );
-
-    PageId allocatePage(
-        PageId page_id,
-        bool dirty
-    );
-
-    void disposePage(
-        PageId page_id,
-        bool dirty
-    );
-
-    void flushFile();
+    void flushPage();
 
 private:
+    FrameId advanceClock();
+    std::optional<FrameId> findVictimFrame();
+    FrameId allocateFrame();
+    PageId allocatePage();
+
     DiskManager& disk_manager_;
+
+    std::size_t pool_size_;
+    std::vector<Frame> frames_;
+    std::vector<FrameId> free_list_;
+    FrameId clock_hand_ = 0;
 
     PageId next_page_id_ = 0;
 
-    std::unordered_map<
-        PageId,
-        Frame
-    > pages_;
+    std::unordered_map<PageId, FrameId> page_table_;
 };
